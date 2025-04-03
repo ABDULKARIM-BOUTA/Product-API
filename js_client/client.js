95df09bc0ae4176de70810144ec9540e6394bd5f
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-04-03 20:44:32 +0300

    creating json client

---
+++ js_client/client.js
@@ -0,0 +1,33 @@
+const loginForm = document.getElementById('login-form')
+const baseEndpoint = "http://localhost:8000/api"
+if (loginForm) {
+    // handle this login form
+    loginForm.addEventListener('submit', handleLogin)
+    }
+
+function handleLogin(event) {
+    event.preventDefault()
+    const loginEndpoint = `${baseEndpoint}/token/`
+
+    let loginFormData = new FormData(loginForm)
+    let loginObjectData = Object.fromEntries(loginFormData)
+    let bodyStr = JSON.stringify(loginObjectData)
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: bodyStr
+    }
+    fetch(loginEndpoint, options) 
+    .then(response=>{
+        return response.json()
+    })
+    .then(authData => {
+        handleAuthData(authData, getProductList)
+    })
+    .catch(err=> {
+        console.log('err', err)
+    })
+}
