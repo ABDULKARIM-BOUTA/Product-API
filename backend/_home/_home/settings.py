bc6378c0211b01a5d18262829452ed22bcdb4e03
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-04-06 13:17:58 +0300

    limit requsets per minute

--- backend/_home/_home/settings.py
+++ backend/_home/_home/settings.py
@@ -140,6 +140,14 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
                                 'PAGE_SIZE': 5,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {       # limit requests
+        'anon': '5/minute',
+        'user': '10/minute',
+    },
 }
 
 ALGOLIA = {
