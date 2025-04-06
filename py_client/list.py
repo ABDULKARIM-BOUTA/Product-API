86555e39b1908000b97d67d8ac5298fd780bd79d
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-04-06 11:28:29 +0300

    creating requirements.txt

--- py_client/list.py
+++ py_client/list.py
@@ -9,7 +9,7 @@ print(auth_response.json())
 
 if auth_response.status_code == 200:
     token = auth_response.json()['token']
-    headers = { 'Authorization': f'Token {token}'}
+    headers = { 'Authorization': f'Bearer {token}'}
 
     endpoint = 'http://127.0.0.1:8000/api/product/list-create/'
     response = requests.get(endpoint, headers=headers)
