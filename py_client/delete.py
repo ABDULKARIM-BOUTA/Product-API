fa6294f499c6fb23be34355ef75398faed5d77ee
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-04-06 23:25:50 +0300

    fixing create, delete, detail, and update files

--- py_client/delete.py
+++ py_client/delete.py
@@ -1,5 +1,24 @@
 import requests
 
-endpoint = 'http://127.0.0.1:8000/api/product/3/delete/'
-response = requests.delete(endpoint)
-print(response.status_code)
+auth_endpoint = 'http://127.0.0.1:8000/api/auth/'
+username = input('Username:')
+password = input('Password:')
+
+auth_response = requests.post(auth_endpoint, json={'username':username, 'password':password})
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = { 'Authorization': f'token {token}'}
+
+    item_pk = 9        # replace item_pk with the desired item pk
+    endpoint = f'http://127.0.0.1:8000/api/product/{item_pk}/delete/'
+    response = requests.delete(endpoint, headers=headers)
+
+    if response.status_code == 204:
+        print("Delete successful:", response.status_code)
+    else:
+        print(f"Delete failed. Status code: {response.status_code}")
+        print("Raw response:", response.text)
+else:
+    print("Authentication failed:", auth_response.json())
