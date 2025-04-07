b7d55f0c109a3c5cb55b5306c7de39e419b7cbaa
Kareem Boutah <TheNamelessOne@tutamail.com>
2025-04-07 16:59:14 +0300

    renaming user model

--- backend/_home/_home/settings.py
+++ backend/_home/_home/settings.py
@@ -164,9 +164,12 @@ PASSWORD_HASHERS = [
 'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
 ]
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = 'users.User'
 
-
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'users.backends.EmailBackend',
+]
 
 # SECURE_BROWSER_XSS_FILTER = True
 # SECURE_CONTENT_TYPE_NOSNIFF = True
